6a64576a542929a2c9a3f8e08fbb8acda51c1347
david-kocharyan <david@aimtech.am>
2019-12-06 19:15:11 +0400

    css fix

--- application/views/site/schools.php
+++ application/views/site/schools.php
@@ -114,7 +114,24 @@
             controls: false,
             item: 4,
             auto: true,
-            loop: true
+            loop: true,
+            responsive : [
+                {
+                    breakpoint:800,
+                    settings: {
+                        item:3,
+                        slideMove:1,
+                        slideMargin:6,
+                    }
+                },
+                {
+                    breakpoint:480,
+                    settings: {
+                        item:1,
+                        slideMove:1
+                    }
+                }
+            ]
 
         });
         $('.slideControls .slidePrev').click(function () {
