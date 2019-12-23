fc76605b8140c85e85c479b3c7824b1b3e47dcbb
david-kocharyan <david@aimtech.am>
2019-12-23 15:43:06 +0400

    fix logo

--- application/controllers/admin/Admins.php
+++ application/controllers/admin/Admins.php
@@ -66,18 +66,6 @@ class Admins extends CI_Controller
 		if ($this->form_validation->run() == FALSE) {
 			$this->settings();
 		} else {
-			if (!empty($_FILES['logo']['name']) || null != $_FILES['logo']['name']) {
-
-				$image = $this->uploadImage('logo');
-				if (isset($image['error'])) {
-					$this->session->set_flashdata('error', $image['error']);
-					$this->settings();
-					return;
-				}
-				$logo = isset($image['data']['file_name']) ? $image['data']['file_name'] : "";
-				unlink(FCPATH . "/plugins/images/Logo/" . $user->logo);
-				unlink(FCPATH . "/plugins/thumb_images/Logo/Thumb_" . $user->logo);
-			}
 
 			$admin = array(
 				'username' => $username,
@@ -86,7 +74,6 @@ class Admins extends CI_Controller
 				'mobile_number' => $mobile_number,
 				'email' => $email,
 			);
-			if (isset($logo)) $admin['logo'] = $logo;
 			if (isset($password)) $admin['password'] = hash("SHA512", $password);
 
 
@@ -97,73 +84,5 @@ class Admins extends CI_Controller
 		}
 	}
 
-	private function uploadImage($image)
-	{
-		if (!is_dir(FCPATH . "/plugins/images/Logo")) {
-			mkdir(FCPATH . "/plugins/images/Logo", 0755, true);
-		}
 
-		if (!is_dir(FCPATH . "/plugins/thumb_images/Logo")) {
-			mkdir(FCPATH . "/plugins/thumb_images/Logo", 0755, true);
-		}
-
-		$path = FCPATH . "/plugins/images/Logo";
-		$config['upload_path'] = $path;
-		$config['file_name'] = 'Logo_' . time() . '_' . rand();
-		$config['allowed_types'] = 'jpg|png|jpeg';
-		$config['max_size'] = 100000;
-		$this->load->library('upload', $config);
-
-		if (!$this->upload->do_upload($image)) {
-			$errorStrings = strip_tags($this->upload->display_errors());
-			$error = array('error' => $errorStrings, 'image' => $image);
-			return $error;
-		} else {
-			$uploadedImage = $this->upload->data();
-			$this->resizeImage($uploadedImage['file_name'], $path);
-
-			$data = array('data' => $uploadedImage);
-			return $data;
-		}
-	}
-
-	private function resizeImage($filename, $path)
-	{
-		$source_path = $path . "/" . $filename;
-		$target_path = $path . "/" . $filename;
-		$config_manip = array(
-			'image_library' => 'gd2',
-			'source_image' => $source_path,
-			'new_image' => $target_path,
-			'maintain_ratio' => TRUE,
-			'create_thumb' => FALSE,
-			'width' => 800,
-			'height' => 800,
-		);
-		$this->load->library('image_lib');
-		$this->image_lib->initialize($config_manip);
-
-		if (!$this->image_lib->resize()) {
-			echo $this->image_lib->display_errors();
-		}
-		$this->image_lib->clear();
-
-//		second thumb resize
-		$source_path = $path . "/" . $filename;
-		$config_manip = array(
-			'image_library' => 'gd2',
-			'source_image' => $source_path,
-			'new_image' => FCPATH . "/plugins/thumb_images/Logo/" . "Thumb_" . $filename,
-			'maintain_ratio' => TRUE,
-			'create_thumb' => FALSE,
-			'width' => 300,
-			'height' => 300,
-		);
-
-		$this->image_lib->initialize($config_manip);
-		if (!$this->image_lib->resize()) {
-			echo $this->image_lib->display_errors();
-		}
-		$this->image_lib->clear();
-	}
 }
